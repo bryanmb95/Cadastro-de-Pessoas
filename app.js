02500ad42dc154b89073540606b747a7f646d5eb
bryanmb95 <39245387+bryanmb95@users.noreply.github.com>
2020-03-10 21:49:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-let dev_db_url = 'mongodb+srv://bryanmb:bryanmb1995@cluster0-jjj5z.mongodb.net/test?retryWrites=true&w=majority'
+let dev_db_url = '<mongoUrl>'
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
